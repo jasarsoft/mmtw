52a737ec3b1ad0034fb4713fb2a0e45ff43c19f1
Edin Jašarević <edinjasar14@gmail.com>
2017-08-09 15:56:20 +0200

    create option tag class

---
+++ Rome Total War/Class/Option/OptionTag.cs
@@ -0,0 +1,39 @@
+﻿
+namespace Jasarsoft.ModManager.TotalWar.Rome
+{
+    class OptionTag
+    {
+        private readonly string name;
+        private readonly string info;
+        private readonly string param;
+        private readonly string state;
+
+        public OptionTag()
+        {
+            this.name = "name";
+            this.info = "info";
+            this.param = "param";
+            this.state = "state";
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+        }
+
+        public string Info
+        {
+            get { return this.info; }
+        }
+
+        public string Param
+        {
+            get { return this.param; }
+        }
+
+        public string State
+        {
+            get { return this.state; }
+        }
+    }
+}
