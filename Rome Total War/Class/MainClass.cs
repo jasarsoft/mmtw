e37dfd3870e404a4d0039a95ea4b9cb9a52c6587
Edin Jašarević <edinjasar14@gmail.com>
2017-08-09 15:18:56 +0200

    rename main class

--- Rome Total War/Class/MainClass.cs
+++ Rome Total War/Class/MainClass.cs
@@ -5,7 +5,7 @@ using System.Windows.Forms;
 
 namespace Jasarsoft.ModManager.TotalWar.Rome
 {
-    static class Program
+    static class MainClass
     {
         /// <summary>
         /// The main entry point for the application.
